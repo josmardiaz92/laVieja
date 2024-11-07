0d1598b97275bf9def623eb7ad078497e1918cdc
josmardiaz92 <josmardiaz92@gmail.com>
2024-11-07 17:13:57 -0400

    menu listo

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <iomanip>
+#include <limits>
 using namespace std;
 
 char tablero[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
@@ -19,21 +20,23 @@ void limpiarPantalla() {
 // Función para mostrar el tablero
 void mostrarTablero() {
     limpiarPantalla();
-    cout << "-------------" << endl;
+    int espacios = (3 * 15 - 3) / 2;
+    
+    cout << setw(espacios) << "" << "-------------" << endl;
     for (int i = 0; i < 3; i++) {
-        cout << "| ";
+        cout <<setw(espacios)<< "" << "| ";
         for (int j = 0; j < 3; j++) {
             cout << tablero[i][j] << " | ";
         }
         cout << endl;
-        cout << "-------------" << endl;
+        cout <<setw(espacios)<< "" << "-------------" << endl;
     }
 }
 
 // Función para tomar la entrada del usuario
 void tomarEntradaJugador() {
     int posicion;
-    cout << "Jugador " << jugadorActual << ", ingresa una posición (1-9): ";
+    cout << "Jugador " << jugadorActual << ", ingresa una posicion (1-9): ";
     cin >> posicion;
 
     // Convertir la posición a coordenadas del tablero
@@ -80,7 +83,42 @@ bool verificarEmpate() {
     return true;
 }
 
-int main() {
+void instrucciones(){
+    limpiarPantalla();
+    cout<<"\t\tInstrucciones del juego\n";
+
+    cout<<"\n1. Selecciona con que ficha quieres jugar (X o O)."<<endl;
+    cout<<"2. Selecciona en tu teclado el nùmero de la casilla en donde quieres colocar tu ficha."<<endl;
+	cout<<"3. Ve colocando tus fichas por turno intentado bloquear a tu oponente."<<endl;
+	cout<<"4. Cuando logres colocar tres fichas en fila habras ganado la partida."<<endl;
+	cout<< "\nPresiona Enter para volver al menú principal."<<endl;
+
+    cin.ignore(); // Para limpiar el buffer de entrada
+    cin.get(); // Esperar a que el usuario presione Enter
+
+}
+
+void creditos(){
+    limpiarPantalla();
+    cout <<"\t\t\tCreditos:\n";
+
+    cout<<"\nDesarrollado por:";
+    cout<<"\n\t Nombre"<<endl;
+    cout<<"\n\t Nombre"<<endl;
+    cout<<"\n\t Nombre"<<endl;
+    cout<<"\n\t Nombre"<<endl;
+    cout<<"\n\t Nombre"<<endl;
+    
+    cout<<"\n\t\tGracias por jugar!\n";
+    
+    cout<<"\nPresiona Enter para volver al menú principal.\n";
+
+    cin.ignore(); // Para limpiar el buffer de entrada
+    cin.get(); // Esperar a que el usuario presione Enter
+
+}
+
+void jugar(){
     char jugarDeNuevo;
     do {
         ganador = false;
@@ -116,7 +154,55 @@ int main() {
 
         cout << "¿Quieres jugar otra partida? (s/n): ";
         cin >> jugarDeNuevo;
-    } while (jugarDeNuevo == 's' || jugarDeNuevo == 'S');
+    } while (jugarDeNuevo == 's' || jugarDeNuevo == 'S');    
+}
 
+void pantallaPrincipal(){
+    int opcion=0;
+
+    do
+    {
+        limpiarPantalla();
+        cout <<"\t\t\tLa Vieja\n";
+        cout<<"\n\t 1. Iniciar Juego"<<endl;
+        cout<<"\n\t 2. Instrucciones"<<endl;
+        cout<<"\n\t 3. Creditos"<<endl;
+        cout<<"\n\t 4. Salir"<<endl;
+        cout<<"\n\n Seleccione una opcion: ";
+        cin>>opcion;
+
+        if(cin.fail() || opcion<=0 || opcion>4){
+            cout << "Opción inválida, por favor elija una opción del 1 al 4.";
+            cin.clear(); // Restablece el estado del `cin`
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Limpia el buffer de entrada
+            cout << "\nPresiona Enter para continuar...";
+            cin.get(); // Espera que el usuario presione Enter
+            continue; // Vuelve al inicio del bucle sin limpiar la pantalla
+        }
+        switch (opcion)
+        {
+            case 1:
+                jugar();
+                break;
+            case 2:
+                instrucciones();
+                break;
+            case 3:
+                creditos();
+                break;
+            case 4:
+                cout << "Saliendo del juego. ¡Hasta luego!" << endl;
+                return;
+                break;
+            default:
+                cout << "Opción no válida. Intenta de nuevo." << endl;
+                break;
+        }
+
+    } while (true);
+}
+
+int main() {
+    pantallaPrincipal();
     return 0;
 }
